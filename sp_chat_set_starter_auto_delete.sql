18a9727069eb757746a093ac864095ff65115a08
mohsen <mohsenmoqadam@yahoo.com>
2023-06-03 17:02:19 +0330

    Bugfix & Add new APIs

---
+++ sp_chat_set_starter_auto_delete.sql
@@ -0,0 +1,43 @@
+DELIMITER %
+DROP PROCEDURE IF EXISTS `luna_dev_db`.`sp_chat_set_starter_auto_delete`%
+CREATE PROCEDURE `luna_dev_db`.`sp_chat_set_starter_auto_delete`(
+	  IN cid_ BIGINT
+	, IN auto_delete_ INT
+)
+BEGIN
+	-- RC:
+	-- 1: EXCEPTION
+	-- 2: DONE	
+	 
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+		SELECT 1 AS 'RC';
+	END;
+   
+	-- Be careful that the latest version of chat_meta is available in the cache layer and we trust it,
+	-- Therefore, in order to reduce the processing load, we do not check the correctness of the information again.
+
+   	SET @mda = NOW();
+   
+	-- TRANSACTION: START
+	START TRANSACTION;
+	-- Update chat_meta
+	UPDATE luna_dev_db.chat_meta 
+		SET starter_auto_delete = auto_delete_ 
+		  , mda = @mda 
+	WHERE cid = cid_;
+	-- Update chat_uid2cid 
+	UPDATE luna_dev_db.chat_uid2cid SET mda = @mda WHERE cid = cid_;
+	-- TRANSACTION: END
+	COMMIT;
+
+	-- Return result.
+	SELECT 2 AS 'RC', @mda AS 'MDA';
+		
+END
+%
+DELIMITER ;
+
+-- EXAMPLE:
+-- CALL `luna_dev_db`.`sp_chat_set_starter_auto_delete`(6, 60);
---
+++ sp_chat_set_starter_muted_state.sql
@@ -0,0 +1,43 @@
+DELIMITER %
+DROP PROCEDURE IF EXISTS `luna_dev_db`.`sp_chat_set_starter_muted_state`%
+CREATE PROCEDURE `luna_dev_db`.`sp_chat_set_starter_muted_state`(
+	  IN cid_ BIGINT
+	, IN is_muted_ BOOL
+)
+BEGIN
+	-- RC:
+	-- 1: EXCEPTION
+	-- 2: DONE	
+	 
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+		SELECT 1 AS 'RC';
+	END;
+   
+	-- Be careful that the latest version of chat_meta is available in the cache layer and we trust it,
+	-- Therefore, in order to reduce the processing load, we do not check the correctness of the information again.
+
+   	SET @mda = NOW();
+   
+	-- TRANSACTION: START
+	START TRANSACTION;
+	-- Update chat_meta
+	UPDATE luna_dev_db.chat_meta 
+		SET starter_is_muted  = is_muted_ 
+		  , mda = @mda 
+	WHERE cid = cid_;
+	-- Update chat_uid2cid 
+	UPDATE luna_dev_db.chat_uid2cid SET mda = @mda WHERE cid = cid_;
+	-- TRANSACTION: END
+	COMMIT;
+
+	-- Return result.
+	SELECT 2 AS 'RC', @mda AS 'MDA';
+		
+END
+%
+DELIMITER ;
+
+-- EXAMPLE:
+-- CALL `luna_dev_db`.`sp_chat_set_starter_muted_state`(6, FALSE);
